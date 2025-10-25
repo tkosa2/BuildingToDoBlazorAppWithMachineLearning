54af6a8ce60b708c4614de065ad49ca6fe86db6d
Trevor Kosa <tkosa3@gmail.com>
2025-10-25 15:20:10 -0700

    Added Blazor app with machine learning

---
+++ BuildingToDoBlazorWithML/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildingToDoBlazorWithML.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public string? PredictedCategory { get; set; } // "Home" or "Work"
+    }
+}
